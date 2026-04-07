41ae506cfeb6cce3353fece9c248dd40fd7ec3ff
Dipak Deshmukh <121744826+dipakdeshmukh9@users.noreply.github.com>
2026-04-07 10:27:26 +0530

    Remove CORS proxy from fetch URL

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,7 +20,7 @@ const Body = () => {
 
     const fetchData = async () => {
       const data = await fetch (
-         "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 );
       const json = await data.json();
       //console.log(json);
